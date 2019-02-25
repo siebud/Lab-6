f6bba500997031ed1f553b92b695f7cf2db61392
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-02-24 22:13:26 -0600

    ShapeSorter methods completed

--- ShapeSorter.java
+++ ShapeSorter.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 /**
@@ -23,6 +24,7 @@ public class ShapeSorter
 	public ShapeSorter()
 	{
 		// TODO: complete this...
+		shapes = new ArrayList<Shape>();
 	}
 
 	/**
@@ -33,6 +35,7 @@ public class ShapeSorter
 	public void addShape(Shape s)
 	{
 		// TODO: complete this...
+		shapes.add(s);
 	}
 
 	/**
@@ -43,6 +46,7 @@ public class ShapeSorter
 	public void sortShapes()
 	{
 		// TODO: complete this...
+		Collections.sort(shapes);
 	}
 
 	/**
@@ -56,6 +60,7 @@ public class ShapeSorter
 	public void sortShapes(Comparator<Shape> comparator)
 	{
 		// TODO: complete this...
+		Collections.sort(shapes, comparator);
 	}
 
 	/**
@@ -68,6 +73,10 @@ public class ShapeSorter
 	public String toString()
 	{
 		// TODO: complete this...
+		String ans = "";
+		for (int i = 0; i < shapes.size(); i++)
+			ans = ans + "\n" + shapes.get(i).toString();
+		return ans;
 	}
 
 }
