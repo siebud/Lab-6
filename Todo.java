5819adb2868a303909f1503f65c134fe2ceb7566
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-02-24 20:16:50 -0600

    todo created

---
+++ Todo.java
@@ -0,0 +1,45 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-02-21
+OU ID_______________: 113471919
+OU EMAIL____________: sierra.j.budinoff-1@ou.edu
+
+Tasks:
+1. getPerimeter method Polygon
+2. circle constructor
+3. circle getPerimeter
+4. circle getShape Type
+5. trapezoid constructor
+6. trapezoid getShapeType
+7. Shape compare
+8. polygon perimeter
+9. equlateral triangle constructor
+10. equilateral triangle area
+11. equilateral triangle type
+
+Estimated Time (minutes):
+1. 10
+2. 15
+3. 10
+4. 15
+5. 10
+6. 10
+7. 50
+8. 30
+9. 20
+10. 15
+11. 20
+
+Actual Time (minutes):
+1. xxx
+2. xxx
+3. 
+4. 
+5. 
+6. 
+7. 
+8. 
+9. 
+10. 
+11. 
+*/}
